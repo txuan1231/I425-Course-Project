dc95b9fbcae27aecc223b04f70fe56df9853038f
FudgyWudgy27 <Zer042711@gmail.com>
2025-06-23 13:23:37 -0400

    Created the sign in, sign up, and sign out pages

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -9,18 +9,30 @@ import {BrowserRouter,Routes,Route}from "react-router-dom";
 import Layout from "../components/Layout";
 import Home from "../pages/home";
 import NoMatch from "../pages/nomatch";
+//Put other imports here:
+
+import {AuthProvider} from "../services/useAuth";
+import Signin from "../pages/auth/signin";
+import Signout from "../pages/auth/signout";
+import Signup from "../pages/auth/signup";
+import RequireAuth from "../components/RequireAuth";
 
 
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout/>}>
-                    <Route index element={<Home/>}/>
-                    <Route path="*" element={<NoMatch/>}/>
-                </Route>
-            </Routes>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/" element={<Layout/>}>
+                        <Route index element={<Home/>}/>
+                        <Route path="/signin" element={<Signin/>}/>
+                        <Route path="/signout" element={<Signout/>}/>
+                        <Route path="/signup" element={<Signup/>}/>
+                        <Route path="*" element={<NoMatch/>}/>
+                    </Route>
+                </Routes>
+            </AuthProvider>
         </BrowserRouter>
     );
 };
